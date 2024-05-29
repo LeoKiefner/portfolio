d5bfd620e83dc977b8aeed327fd93069ecdf166c
Leo Kiefner <leokiefner.pro@gmail.com>
2024-05-29 20:13:05 +0200

    Initial commit

---
+++ src/components/About/About.js
@@ -0,0 +1,69 @@
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import { about } from '../../portfolio'
+import './About.css'
+
+const About = () => {
+  const { name, role, description, resume, social, photo } = about
+
+  return (
+    <div className='about'>
+      <div className='about__content'>
+        {name && (
+          <>
+            <h1>
+              <span className='about__title'>A FULL STACK WEB DEVELOPPER ALWAYS READY TO LEARN</span>
+            </h1>
+            <h2>
+              <span className='about__name'>{name}</span>
+            </h2>
+          </>
+        )}
+
+        {role && <h2 className='about__role'>{role}.</h2>}
+        <p className='about__desc'>{description && description}</p>
+
+        <div className='about__contact'>
+          {resume && (
+            <a href={resume}>
+              <span type='button' className='btn btn--outline'>
+                Resume
+              </span>
+            </a>
+          )}
+
+          {social && (
+            <>
+              {social.github && (
+                <a
+                  href={social.github}
+                  aria-label='github'
+                  className='link link--icon'
+                >
+                  <GitHubIcon />
+                </a>
+              )}
+
+              {social.linkedin && (
+                <a
+                  href={social.linkedin}
+                  aria-label='linkedin'
+                  className='link link--icon'
+                >
+                  <LinkedInIcon />
+                </a>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+      {photo && (
+        <div className='about__photo-container'>
+          <img src={photo} alt='Profile' className='about__photo' />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default About
---
+++ src/portfolio.js
@@ -0,0 +1,70 @@
+const header = {
+  homepage: '',
+  title: 'Léo Kiefner',
+}
+
+const about = {
+  name: 'Léo Kiefner',
+  role: 'Working @ C-Koya Tech',
+  description:
+  'I am a dedicated second-year Computer Science student at IUT Robert Schuman, specializing in software solution development with a keen passion for web development.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/leo-kiefner/',
+    github: 'https://github.com/LeoKiefner'
+  },
+  photo: 'https://cdn.discordapp.com/attachments/1221561089632895198/1245419565261914233/1699710973598.png?ex=6658aedb&is=66575d5b&hm=109ce61a4b1f9f5fc645842dface18db800d556c15fddf10df8fca478845c93d&',
+}
+
+const projects = [ 
+  {
+    image: 'https://media.discordapp.net/attachments/1221561089632895198/1245116127584190495/file-vwFiwvxpMK8bHfFSQrnLi0wq.png?ex=66579441&is=665642c1&hm=7530dadb336f4c5ea3c4eeba0300c38e8dadb353aa0ef6439453d3229380d7e8&=&format=webp&quality=lossless&width=693&height=693 ',
+    name: 'CocoBot',
+    description: 'Cocobot is an innovative solution designed with a friend of mine to revolutionize content creation across platforms. This automated system takes a video uploaded to YouTube via a link and edits it to align with the algorithmic preferences of TikTok and YouTube Shorts.',
+    stack: ['python', 'api', 'automation'],
+    sourceCode: 'https://github.com/LeoKiefner/CocoBot-Automatic-Editing-Video-App/tree/main',
+    livePreview: 'https://github.com/LeoKiefner/CocoBot-Automatic-Editing-Video-App/tree/main',
+  },
+  {
+    image: 'https://media.discordapp.net/attachments/1221561089632895198/1245113932096475176/file-jhpyUyMJJlaAUjdPLa7XrzeR.png?ex=66579236&is=665640b6&hm=92117ec6a47e3a1caa983c5f4ed7f14219689ad4cb66728fbf3754aa05327a31&=&format=webp&quality=lossless&width=350&height=350',
+    name: 'Bookology',
+    description: 'React and Laravel project to enhance and gamify your reading experience',
+    stack: ['React', 'TypeScript', 'JavaScript', 'Laravel', 'PHP', 'SQL'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    image: 'https://cdn.discordapp.com/attachments/1221561089632895198/1245115861363069068/file-8i3hliCiTXwh8mkWVCNRTgSj.png?ex=66579402&is=66564282&hm=1a6ac82b63537f1596adb5c1928075bf8f417b7e4149720e2e4608854053ced7&',
+    name: 'MoseArt',
+    description: 'MoseArt is a turn-based management game in which the player embodies Robert Mose, the director of the IUT Robert Schuman in Illkirch-Graffenstaden, for a 5-year mandate.',
+    stack: ['Unity', 'C#', 'Plastic SCM','Agile', 'Scrum'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'Laravel',
+  'Symfony',
+  'PHP',
+  'Git',
+  'SQL',
+  'Python',
+  'C#',
+  'C',
+  'Unity',
+  'Java',
+  'R',
+  'HTML',
+  'CSS',
+]
+
+const contact = {
+  email: 'leokiefner.pro@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
